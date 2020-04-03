e84d9e024e999598cf15aaf66d819041d8ee2b45
Alphonse <alphonso2009@gmail.com>
2020-04-03 21:29:51 +0300

    Migrations and Models

--- database/migrations/2020_04_03_173000_create_tasks_table.php
+++ database/migrations/2020_04_03_173000_create_tasks_table.php
@@ -15,6 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('completed');
             $table->timestamps();
         });
     }
